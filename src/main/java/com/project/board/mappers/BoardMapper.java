0f88452c5ff696b4313f97e7e5a56f23c0b61997
KimTaerin <chloekim66@intellivix.com>
2022-11-03 13:43:14 +0900

    FIX - repository > mapper로 변경, saveDBGroups 추가

--- src/main/java/com/project/board/mappers/BoardMapper.java
+++ src/main/java/com/project/board/mappers/BoardMapper.java
@@ -1,8 +1,8 @@
-package com.project.board.repository;
+package com.project.board.mappers;
 
 
 import com.project.board.domain.Board;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
@@ -12,18 +12,23 @@ import java.util.List;
  * @author kimtaerin66
  *
  * */
-@Repository
-public interface BoardRepository {
+@Mapper
+public interface BoardMapper {
 
 
     //게시판 전체 조회
-    List<Board.Response> getBoardDBList();
+    List<Board.Response> getBoardDBList(Board.Search searchParam);
 
     //게시물 하나(상세) 조회
     Board.Response getDBBoard(int boardSeq);
 
+
     //게시물 추가
     void saveDBBoard(Board.Request board);
+
+    //게시물 여러개 추가
+    int saveDBGroups(List<Board.Request> param);
+
 
     //게시물 수정
     void updateDBBoard(Board.Request board);
