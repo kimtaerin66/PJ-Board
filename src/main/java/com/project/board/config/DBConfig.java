e7e4b947ae07e951f75103139edb3788c7b0d28f
KimTaerin <chloekim66@intellivix.com>
2022-11-07 08:43:33 +0900

    FIX - DB config.java + Mybatis Config.java

--- src/main/java/com/project/board/config/DBConfig.java
+++ src/main/java/com/project/board/config/DBConfig.java
@@ -1,18 +1,49 @@
 package com.project.board.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
+@MapperScan(basePackages = "com.project.board.mappers", sqlSessionFactoryRef="SqlSessionFactory")
+@EnableTransactionManagement
 public class DBConfig {
 
-    @Bean
+    @Primary
+    @Bean(name = "dataSource")
     @ConfigurationProperties(prefix = "spring.datasource") //설정에 관련된것을 spring.datasource에서 찾겠다.
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
+
+    @Primary
+    @Bean(name = "SqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource, ApplicationContext applicationContext) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:config/mybatis-config.xml"));
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:mappers/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+    @Primary
+    @Bean(name = "SqlSessionTemplate")
+    public SqlSessionTemplate SqlSessionTemplate(@Qualifier("SqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws IOException {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+
 }
