64dc6fedb15bcce05e14f2ea0d5ef7813d4a4800
Taerin <85108615+kimtaerin66@users.noreply.github.com>
2022-11-14 16:03:50 +0900

    Add files via upload

---
+++ vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <transition name="fade-view" mode="out-in">
+    <router-view @updatePage="updatePage"></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data() {
+    return {
+      currentPath: '',
+    }
+  },
+  methods: {
+    updatePage(value) {
+      this.currentPath = value;
+    },
+  }
+};
+</script>
---
+++ vue/src/main.js
@@ -0,0 +1,14 @@
+ import '@/assets/board.scss';
+
+import Vue from "vue";
+import App from "./App.vue";
+import { router } from './router';
+
+Vue.config.productionTip = false;
+
+const app = new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app');
+
+window.app = app;
