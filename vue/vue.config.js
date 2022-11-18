362a69925aebbac86da07d114bc5f94fbf6c6942
KimTaerin <chloekim66@intellivix.com>
2022-11-18 16:21:00 +0900

    springboot와 연동

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-    devServer: {
-        port: 80
+  outputDir: '../src/main/resources/static', // 빌드 타겟 디렉토리
+  devServer: {
+    proxy: {
+      '/api': {
+        // '/api' 로 들어오면 포트 8080(스프링 서버)로 보낸다
+        target: 'http://localhost:8080',
+        changeOrigin: true, // cross origin 허용
+      },
     },
-    css : {
-        loaderOptions:{
-            sass :{
-            }
-        }
-
-    }
-
-
+  },
 };
