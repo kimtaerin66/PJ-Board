aa492d8d9f013526d6fdfe6c23a1cff4d11fe63c
KimTaerin <chloekim66@intellivix.com>
2022-11-23 15:24:14 +0900

    ADD - 현재 위치 나타내는 Location 추가

---
+++ vue/src/common/LocationCom.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="location-wrap">
+    <div class="location-box">
+      <ul class="location">
+        <li>Home</li>
+        <li v-for="(item, index) in location" :key="index">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      location: ['전자결재', '결재중'],
+    };
+  },
+};
+</script>
